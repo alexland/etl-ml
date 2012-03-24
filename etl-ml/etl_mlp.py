e9841ad34b555ca35e4ff861410ce2576c675e83
doug y'barbo <doug.ybarbo@gmail.com>
2012-03-24 14:57:28 -0700

    rewrote etl_mlp.py as single main function

--- etl-ml/etl_mlp.py
+++ etl-ml/etl_mlp.py
@@ -1,12 +1,12 @@
 #!/usr/local/bin/python
 # encoding: utf-8
 
-# TODO: add 'converters' to NP.loadtxt call in case class labels are strings
+
 
 import sys
 import os
 import random
-import Collections as CL
+import collections as CL
 import itettools as IT
 import numpy as NP
 
@@ -57,13 +57,43 @@ data = NP.apply_along_axis(rescale_mlp, axis=0, arr=data)
 nrows = data.shape[0]
 TEST_FRACTION = .1
 VALIDATE_FRACTION = .1
-
 size_validate = NP.floor(VALIDATE_FRACTION * nrows)
 size_test = NP.floor(TEST_FRACTION * nrows)
 size_train = nrows - (size_validate + size_test)
-
 train_data = data[:size_train,:]
 validate_data = data[ size_train : size_train+size_validate,: ]
 test_data = data[ size_train+size_validate:,:]
+
+
+def etl_mlp(dfile):
+    with open(dfile, 'r') as f:
+    	r1 = f.readline().strip().split(',')
+    try:
+    	int(r1[-1])
+    	# proceed w/ simple case--class labels are integers
+    	D = NP.loadtxt(dfile, delimiter=",", skiprows=0, comments='#')
+    except ValueError:
+    	# class labels are strings
+    	file_obj = open(dfile, 'r')
+    	data = [ row.strip().split(',') for row in file_obj.readlines() ]
+    	class_labels = {row[-1] for row in data[1:]}
+    	tx = [ label for label in enumerate(class_labels)]
+    	LuT = dict([ (v, k) for k, v in tx ])
+    	fnx = lambda k : LuT[k]
+    	D = NP.loadtxt( dfile, delimiter=",", skiprows=0, comments='#', 
+    		converters={-1:fnx} )
+	NP.random.shuffle(D)
+	data, class_labels = NP.hsplit(D, [-1])
+    data = NP.apply_along_axis(rescale_mlp, axis=0, arr=data)
+	nrows = data.shape[0]
+	TEST_FRACTION = .1
+	VALIDATE_FRACTION = .1
+	size_validate = NP.floor(VALIDATE_FRACTION * nrows)
+	size_test = NP.floor(TEST_FRACTION * nrows)
+	size_train = nrows - (size_validate + size_test)
+	train_data = data[:size_train,:]
+	validate_data = data[ size_train : size_train+size_validate,: ]
+	test_data = data[ size_train+size_validate:,:]
+	return (train_data, test_data, validate_data)
 
 
