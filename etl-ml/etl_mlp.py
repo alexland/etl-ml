c9d4aaa2298c4f40371fb42cde3417739fab4991
doug y'barbo <doug.ybarbo@gmail.com>
2012-03-24 18:22:22 -0700

    etl-mlp.py refactored so main fn returns named tuple

--- etl-ml/etl_mlp.py
+++ etl-ml/etl_mlp.py
@@ -1,8 +1,6 @@
 #!/usr/local/bin/python
 # encoding: utf-8
 
-
-
 import sys
 import os
 import random
@@ -10,33 +8,6 @@ import collections as CL
 import itettools as IT
 import numpy as NP
 
-
-data_path = "/data"
-dfile = os.path.join(data_path, "iris.csv")
-
-with open(dfile, 'r') as f:
-	r1 = f.readline().strip().split(',')
-
-try:
-	int(r1[-1])
-	# proceed w/ simple case--class labels are integers
-	D = NP.loadtxt(dfile, delimiter=",", skiprows=0, comments='#')
-except ValueError:
-	# class labels are strings
-	file_obj = open(dfile, 'r')
-	data = [ row.strip().split(',') for row in file_obj.readlines() ]
-	class_labels = {row[-1] for row in data[1:]}
-	tx = [ label for label in enumerate(class_labels)]
-	LuT = dict([ (v, k) for k, v in tx ])
-	fnx = lambda k : LuT[k]
-	D = NP.loadtxt( dfile, delimiter=",", skiprows=0, comments='#', 
-		converters={-1:fnx} )
-
-
-NP.random.shuffle(D)
-
-# separate the feature vectors & class labvels:
-data, class_labels = NP.hsplit(D, [-1])
 
 # rescale the data so that each feature (column) has a min/max of -1 to 1
 def rescale_mlp(a):
@@ -48,22 +19,6 @@ def rescale_mlp(a):
 	a /= .5*a.max()
 	a -= 1
 	return a
-
-data = NP.apply_along_axis(rescale_mlp, axis=0, arr=data)
-
-# split data into training, test, & validation groups
-# 80-10-10 fractions
-
-nrows = data.shape[0]
-TEST_FRACTION = .1
-VALIDATE_FRACTION = .1
-size_validate = NP.floor(VALIDATE_FRACTION * nrows)
-size_test = NP.floor(TEST_FRACTION * nrows)
-size_train = nrows - (size_validate + size_test)
-train_data = data[:size_train,:]
-validate_data = data[ size_train : size_train+size_validate,: ]
-test_data = data[ size_train+size_validate:,:]
-
 
 def etl_mlp(dfile):
     with open(dfile, 'r') as f:
@@ -84,16 +39,21 @@ def etl_mlp(dfile):
     		converters={-1:fnx} )
 	NP.random.shuffle(D)
 	data, class_labels = NP.hsplit(D, [-1])
-    data = NP.apply_along_axis(rescale_mlp, axis=0, arr=data)
+	data = NP.apply_along_axis(rescale_mlp, axis=0, arr=data)
 	nrows = data.shape[0]
 	TEST_FRACTION = .1
-	VALIDATE_FRACTION = .1
-	size_validate = NP.floor(VALIDATE_FRACTION * nrows)
-	size_test = NP.floor(TEST_FRACTION * nrows)
-	size_train = nrows - (size_validate + size_test)
-	train_data = data[:size_train,:]
-	validate_data = data[ size_train : size_train+size_validate,: ]
-	test_data = data[ size_train+size_validate:,:]
-	return (train_data, test_data, validate_data)
+	ALIDATE_FRACTION = .1
+	offset1 = NP.floor(TEST_FRACTION * nrows)
+	offset2 = NP.floor(VALIDATE_FRACTION * nrows)
+	test_data = data[:offset1,:]
+	test_data_labels = class_labels[:offset1]
+	validate_data_labels = class_labels[offset1:offset1+offset2]
+	validate_data = data[offset1:offset1+offset2,:]
+	train_data = data[offset1+offset2:,:]
+	train_data_labels = class_labels[offset1+offset2:]
+	return CL.namedtuple('mlp_data', [ 'train_data', 'train_data"labels', 
+								'test_data', 'test_data_labels',
+								'validate_data', 'validate_data_labels'], 
+								verbose=True)
 
 
